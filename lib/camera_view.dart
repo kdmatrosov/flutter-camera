142423e0acb8a975c655e0e4f16288c8c2e66827
Kirill Matrosov <k.matrosov@orbita.center>
2019-03-11 20:45:36 +0300

    camera_view update

--- lib/camera_view.dart
+++ lib/camera_view.dart
@@ -1,8 +1,52 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class CameraView extends StatelessWidget {
+class CameraView extends StatefulWidget {
+  @override
+  _CameraViewState createState() => _CameraViewState();
+}
+
+class _CameraViewState extends State<CameraView> {
+  List<CameraDescription> _cameras;
+  CameraController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _getAvailableCameras();
+  }
+
+  Future<void> _getAvailableCameras() async {
+    final cameras = await availableCameras();
+    setState(() {
+      _cameras = cameras;
+      try {
+        _controller = CameraController(cameras[0], ResolutionPreset.medium);
+        _controller.initialize().then((_) {
+          if (!mounted) {
+            return;
+          }
+          setState(() {});
+        });
+      } catch (e) {
+        print(e);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Camera"),);
+    if (_controller == null || !_controller.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+        aspectRatio: _controller.value.aspectRatio,
+        child: CameraPreview(_controller));
   }
 }
