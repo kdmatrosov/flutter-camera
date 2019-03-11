c3a8a3136a7e6becb39fc69147b162304d13a542
Kirill Matrosov <k.matrosov@orbita.center>
2019-03-11 18:24:59 +0300

    init view

---
+++ lib/image_picker_view.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ImagePickerView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("ImagePicker"),);
+  }
+}
